4cf51da8cd8ae6137c4877224857bc23089bced4
Vladimir Danilov <danilov17792@yandex.ru>
2020-03-07 11:09:50 +0300

    Задание 3 готово

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,34 @@
+import java.util.*;
+
+import static java.lang.System.out;
+
+
 public class Main {
     public static void main(String[] args) {
-        
+        String [] FIO = {"Иванов", "Петров", "Сидоров", "Соколов","Иванов", "Петров", "Сидоров", "Соколов", "Данилов", "Данилов"};
+
+        Map<String, Integer> map = new HashMap<>();
+        for (String fio : FIO) {
+            Integer val;
+            val = map.get(fio);
+            if (val == null){
+                map.put(fio, 1);
+            } else {
+                map.put(fio, val + 1);
+            }
+        }
+        Phonebook pb = new Phonebook();
+
+        pb.add("Иванов", "123");
+        pb.add("Петров", "456");
+        pb.add("Петров", "789");
+        pb.add("Иванов", "25123");
+
+
+        out.println(map);
+        out.println(pb.get("Иванов"));
     }
+
+
+
 }
